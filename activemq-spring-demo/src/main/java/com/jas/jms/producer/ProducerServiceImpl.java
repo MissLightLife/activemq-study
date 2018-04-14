c4fba1bd1b455ece9557f6b48ef0898a9fb3e295
corderjas <1012037464@qq.com>
2018-04-14 16:57:00 +0800

    improve some code

--- activemq-spring-demo/src/main/java/com/jas/jms/producer/ProducerServiceImpl.java
+++ activemq-spring-demo/src/main/java/com/jas/jms/producer/ProducerServiceImpl.java
@@ -1,14 +1,11 @@
 package com.jas.jms.producer;
 
-import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Session;
-import javax.jms.TextMessage;
+import javax.annotation.Resource;
+import javax.jms.*;
 
 /**
  * @author Jas
@@ -18,12 +15,16 @@ public class ProducerServiceImpl implements ProducerService {
 
     @Autowired
     JmsTemplate jmsTemplate;
-    @Autowired
-    ActiveMQQueue activeMQQueue;
+    /**
+     * 这里以 @Resource 方式注入目的地对象
+     * 如果是发布者/订阅者模式，只选要修改 name 中的值为“activeMQTopic”即可
+     */
+    @Resource(name = "activeMQQueue")
+    Destination destination;
 
     @Override
     public void sendMessage(final String message) {
-       jmsTemplate.send(activeMQQueue, new MessageCreator() {
+       jmsTemplate.send(destination, new MessageCreator() {
            @Override
            public Message createMessage(Session session) throws JMSException {
                TextMessage textMessage = session.createTextMessage(message);
